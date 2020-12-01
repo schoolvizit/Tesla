54e88fcf0f638ac724ed572e5ca57f70bf068dc8
Andi <galynski@yahoo.com>
2020-12-01 04:28:08 +0300

    button

---
+++ js/main.js
@@ -0,0 +1,63 @@
+// burger js
+
+
+
+const burger = document.querySelector('.burger'),
+      buttonClose = document.querySelector('.burger span'),
+      menu = document.querySelector('.header__menu'),
+      links = document.querySelectorAll('.header__link'),
+
+      // блок перекрывающий контент body
+      overlay = document.querySelector('.burger__overlay');
+
+
+
+burger.addEventListener('click', () => {
+  buttonClose.classList.toggle('active');
+  menu.classList.toggle('active');
+  overlay.classList.toggle('active');
+  document.body.classList.toggle('lock');
+});
+
+
+
+overlay.addEventListener('click', () => {
+  buttonClose.classList.remove('active');
+  menu.classList.remove('active');
+  overlay.classList.remove('active');
+  document.body.classList.remove('lock');
+});
+
+
+links.forEach(function (item) {
+  item.addEventListener('click', () => {
+    buttonClose.classList.remove('active');
+    menu.classList.remove('active');
+    overlay.classList.remove('active');
+    document.body.classList.remove('lock');
+  });
+});
+
+
+// active class of menu items onscroll
+
+
+// window.addEventListener('scroll', () => {
+//   let scrollDistance = window.scrollY;
+  
+
+// 	if (window.innerWidth > 768) {
+// 		document.querySelectorAll('.fullscreen').forEach((el, i) => {
+// 			if (el.offsetTop - document.querySelector('.header__menu').clientHeight <= scrollDistance) {
+// 				document.querySelectorAll('.header__menu a').forEach((el) => {
+// 					if (el.classList.contains('active')) {
+// 						el.classList.remove('active');
+// 					}
+// 				});
+
+// 				document.querySelectorAll('.header__menu li')[i].querySelector('a').classList.add('active');
+// 			}
+// 		});
+// 	}
+// });
+
